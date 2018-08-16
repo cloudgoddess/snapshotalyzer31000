a5edb8dae6232052c7f8e1732da9003ae637738e
Cloudgoddess <happygilmour75@gmail.com>
2018-08-15 18:07:32 -0600

    Added changes to script to get more information from ec2.instance.all()

--- snapshot/snapshotalyzer.py
+++ snapshot/snapshotalyzer.py
@@ -1,9 +1,28 @@
 import boto3
+import click 
+
+
+# Make sure to have access to ec2 each time it is run
+session = boto3.Session(profile_name='snapshotalyzer')
+ec2 = session.resource('ec2')
+
+# Make listing instances as a function
+
+@click.command()
+def list_instances():
+	"List Ec2 instances" # using click --help will give this
+	for i in ec2.instances.all():
+                print(', '.join((
+			i.id,
+			i.instance_type,
+			i.placement['AvailabilityZone'],
+			i.state['Name'],
+			i.public_dns_name)))
+
+	return
 
 if __name__ == '__main__':
-        session = boto3.Session(profile_name='snapshotalyzer')
-        ec2 = session.resource('ec2')
+	list_instances()
 
-        for i in ec2.instances.all():
-                print(i)
+
 
